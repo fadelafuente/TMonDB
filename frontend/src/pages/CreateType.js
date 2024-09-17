9bef65a3eb9707e78136cecfcffa5cfd34e36ede
fadelafuente <f.delafuente@outlook.com>
2024-09-16 20:02:26 -0500

    create a list of types and a way to add to that list

--- frontend/src/pages/CreateType.js
+++ frontend/src/pages/CreateType.js
@@ -1,10 +1,10 @@
 import TitleBar from "../components/TitleBar";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCurrentUserDetails, useNavigateNotAuth } from "../hooks/hooks";
 import { connect } from "react-redux";
 import { Button, Card } from "react-bootstrap";
-import { BsArrowLeft, BsXCircle } from "react-icons/bs";
+import { BsArrowLeft, BsDashCircle, BsPlus, BsPlusCircle } from "react-icons/bs";
 
 import "../assets/styling/content.css";
 import "../assets/styling/types.css";
@@ -12,10 +12,23 @@ import "../assets/styling/types.css";
 function CreateType({ isAuthenticated }) {
     const [query, setQuery] = useState("");
     const [user] = useCurrentUserDetails(isAuthenticated);
-    const params = useParams();
-    const location = useLocation();
     const navigate = useNavigate();
+    const [types, setTypes] = useState(['fire', 'water', 'grass']);
+    const [newType, setNewType] = useState("");
     useNavigateNotAuth(isAuthenticated);
+
+    function addType(t) {
+        if(types.length >= 20)
+            return;
+        if(!types.includes(t))
+            setTypes([...types, t]);
+        setNewType("");
+    }
+
+    function deleteType(n) {
+        const removed = [...types].filter((t) => t !== n);
+        setTypes(removed);
+    }
 
     return (
         <>
@@ -31,12 +44,31 @@ function CreateType({ isAuthenticated }) {
                                 <BsArrowLeft/>
                             </Button>
                             <h4 className="types-h4">
-                                Create New Type
+                                Create New Type(s)
                             </h4>
                         </div>
                     </Card.Header>
-                    <Card.Body className='deleted-icon'>
-                        <BsXCircle/>
+                    <Card.Body>
+                        <div className="type-list">
+                            {
+                                Array.from(types, type => (
+                                    <div className="type-row">
+                                        <div className="type-name">
+                                            {type}
+                                        </div>
+                                        <Button className="svg-btn types-btn remove-btn" onClick={ () => deleteType(type) }>
+                                            <BsDashCircle />
+                                        </Button>
+                                    </div>
+                                ))
+                            }
+                            <div className="type-add">
+                                <input className="type-input" onChange={ e => setNewType(e.target.value) } value={ newType } />
+                                <Button className="svg-btn types-btn" onClick={ () => addType(newType) }>
+                                    <BsPlusCircle/>
+                                </Button>
+                            </div>
+                        </div>
                     </Card.Body>
                     <Card.Footer className="types-footer">
                         <Button 
