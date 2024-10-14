b53b7b96bd222651eb91546b02e3675058a557df
fadelafuente <f.delafuente@outlook.com>
2024-10-14 16:45:42 -0500

    add limiting of types to two types, and added limiting selection to only one specific group.

--- frontend/src/components/Creates/CreateMon.js
+++ frontend/src/components/Creates/CreateMon.js
@@ -4,11 +4,37 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Select from 'react-select';
 
 export default function CreateMon() {
-    const types = ["fire", "water", "grass", "ice", "dragon", "ground", "rock", "electric", "bug", "normal", "fighting", "psychic", "steel", "poison", "flying", "fairy", "dark", "ghost"];
+    /* PLACEHOLDERS!!! Eventually will be replaced with data from backend! */
+    const types = {Pokemon: ["fire", "water", "grass", "ice", "dragon", "ground", "rock", "electric", "bug", "normal", "fighting", "psychic", "steel", "poison", "flying", "dark", "ghost", "fairy"], 
+        Temtem: ["neutral", "wind", "earth", "water", "fire", "nature", "electric", "mental", "digital", "melee", "crystal", "toxic"]};
+    
+    const [groupedTypes, setGroupedTypes] = useState(
+        Object.keys(types).map((key, _) => {
+            const currentGroup = types[key].map(type => MakeOption(type, key));
+            return {label: key, options: currentGroup};
+        })
+    ); 
     const [chosenTypes, setChosenTypes] = useState([]);
 
-    function MakeOption(t) {
-        return {value: t, label: t};
+    function MakeOption(t, g) {
+        return {value: t, label: t, groupName: g};
+    }
+
+    function onTypesChange(e) {
+        if(e.length > 0) { 
+            const groupName = e[0]["groupName"]; 
+            const groupOptions = types[groupName].map(type => MakeOption(type, groupName));
+            setGroupedTypes([{label: groupName, options: groupOptions}]);
+        } else {
+            setGroupedTypes(
+                Object.keys(types).map((key, _) => {
+                    const currentGroup = types[key].map(type => MakeOption(type, key));
+                    return {label: key, options: currentGroup};
+                })
+            );
+        }
+
+        setChosenTypes(e.map((dict, _) => { return dict["value"] }));
     }
 
     return (
@@ -74,8 +100,9 @@ export default function CreateMon() {
                     isMulti 
                     isClearable
                     closeMenuOnSelect={false}
-                    options={ types.map(type => MakeOption(type)) } 
-                    onChange={ e => setChosenTypes(e.map((dict, _) => { return dict["value"] })) }
+                    options={ groupedTypes } 
+                    onChange={ e => onTypesChange(e) }
+                    isOptionDisabled={ () => chosenTypes.length >= 2 }
                 />
             </Form>
         </div>
