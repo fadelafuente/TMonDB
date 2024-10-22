c86003c219c6e116ddce5d9b036b74314a4c220e
fadelafuente <f.delafuente@outlook.com>
2024-10-22 13:54:25 -0500

    fixed styling I accidently broke, and changed how the world information works. Changing the world now resets all of the other world options.

--- frontend/src/components/UserInteractions/SearchMultiSelect.js
+++ frontend/src/components/UserInteractions/SearchMultiSelect.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Select from 'react-select';
 
 export default function SearchMultiSelect({ initialItems, groups=null, chosenItems, setChosenItems, limit=null, isGrouped=false, isMulti=false }) {
